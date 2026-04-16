1f6e852d60ad19c2ec29e5c5794c7df8a710738c
Otibra <otienobrian718@gmail.com>
2026-04-16 13:50:05 -0700

    fix deployment config

--- api_integration_data_processing/api/urls.py
+++ api_integration_data_processing/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import gender_view
 
 urlpatterns = [
-    path('api/classify', gender_view, name='gender-classify'),
+    path('classify', gender_view, name='gender-classify'),
 ]
--- api_integration_data_processing/api_integration_data_processing/settings.py
+++ api_integration_data_processing/api_integration_data_processing/settings.py
@@ -43,10 +43,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CORS_ALLOW_ALL_ORIGINS = True
